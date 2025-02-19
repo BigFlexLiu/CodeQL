5a95448e102cba56f5611b9653402b8b165028f3
Felix Liu <Felixliu135@gmail.com>
2025-02-19 18:21:43 -0500

    Q1Q3

--- sample.c
+++ sample.c
@@ -1,27 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define BUFFSIZE 256
+    
+int read_file(const char *filename) {
+    FILE* ptr = fopen(filename, "r");
+    
+    if (ptr == NULL) {
+        printf("File cannot be opened.\n");
+        return(-1);
+    }
+    
+    char buff[BUFFSIZE];
+    
+    while (fgets(buff, BUFFSIZE, ptr) != NULL) {
+        printf("%s", buff);
+    }
+    
+    return 0; 
+}
+
 int main(int argc, char** argv) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
-        return 1;
-    }
-
-    FILE *file = fopen(argv[1], "rb");  // Open file in binary mode
-    if (!file) {
-        perror("Error opening file");
-        return 1;
-    }
-
-    fseek(file, 0, SEEK_END);  // Move to end of file
-    long filesize = ftell(file);  // Get current position (file size)
-    fclose(file);  // Close the file
-
-    if (filesize == -1) {
-        perror("Error determining file size");
-        return 1;
-    }
-
-    printf("%ld\n", filesize);
-    return 0;
+  read_file("text.txt");
 }
